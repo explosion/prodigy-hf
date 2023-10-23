43b573bca7e6a3c9523c215e5a695732f3f5b330
Vincent <vincent@calmcode.io>
2023-10-23 15:14:39 +0200

    another fix another hour passed

--- tests/test_train_basics.py
+++ tests/test_train_basics.py
@@ -2,4 +2,4 @@ from prodigy_hf import hf_train_ner
 
 
 def test_smoke_ner(tmpdir):
-    hf_train_ner("fashion,eval:fashion", tmpdir, epochs=1, model_name="sshleifer/tiny-distilroberta-base")
+    hf_train_ner("fashion,eval:fashion", tmpdir, epochs=1)
